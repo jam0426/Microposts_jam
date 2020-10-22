f87da4ef26943fca7648c10e41f84ad27253ed4f
uekitaiju <jamjam.taiju@gmail.com>
2020-10-22 08:03:31 +0000

    follow, unfollow

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -3,22 +3,23 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-use App\User;
+
+use App\User; // 追加
 
 class UsersController extends Controller
 {
     public function index()
     {
-        //ユーザー一覧をidの降順で取得
+        // ユーザ一覧をidの降順で取得
         $users = User::orderBy('id', 'desc')->paginate(10);
-        
-        //ユーザー一覧ビューでそれを表示
-        return view('users.index',[
-           'users' => $users, 
+
+        // ユーザ一覧ビューでそれを表示
+        return view('users.index', [
+            'users' => $users,
         ]);
     }
     
-     public function show($id)
+    public function show($id)
     {
         // idの値でユーザを検索して取得
         $user = User::findOrFail($id);
@@ -33,6 +34,42 @@ class UsersController extends Controller
         return view('users.show', [
             'user' => $user,
             'microposts' => $microposts,
+        ]);
+    }
+    
+    public function followings($id)
+    {
+        // idの値でユーザを検索して取得
+        $user = User::findOrFail($id);
+
+        // 関係するモデルの件数をロード
+        $user->loadRelationshipCounts();
+
+        // ユーザのフォロー一覧を取得
+        $followings = $user->followings()->paginate(10);
+
+        // フォロー一覧ビューでそれらを表示
+        return view('users.followings', [
+            'user' => $user,
+            'users' => $followings,
+        ]);
+    }
+
+    public function followers($id)
+    {
+        // idの値でユーザを検索して取得
+        $user = User::findOrFail($id);
+
+        // 関係するモデルの件数をロード
+        $user->loadRelationshipCounts();
+
+        // ユーザのフォロワー一覧を取得
+        $followers = $user->followers()->paginate(10);
+
+        // フォロワー一覧ビューでそれらを表示
+        return view('users.followers', [
+            'user' => $user,
+            'users' => $followers,
         ]);
     }
 }
